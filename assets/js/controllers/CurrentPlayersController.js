fa3f5197a6049f43609788d525fb4d4854dc111c
Benjamin Demolder <benjamin.demolder@gmail.com>
2017-12-19 16:00:04 +0100

    Fix issue with player count string

--- assets/js/controllers/CurrentPlayersController.js
+++ assets/js/controllers/CurrentPlayersController.js
@@ -5,9 +5,9 @@ bgnwebapp.controller('CurrentPlayersController', ['$scope', '$rootScope', '$loca
     $ctrl.boardgamenight = $ctrl.resolve.boardgamenight;
   };
 
-  $scope.getHostPlayerCountString = function () {
+  $ctrl.getHostPlayerCountString = function () {
     var amount = $ctrl.boardgamenight.totalPlayerCount - $ctrl.boardgamenight.availablePlayerCount - 1;
-
-    return amount === 1 ? '1 other.' : amount + ' others.'
+    if (amount === 0) return '';
+    return amount === 1 ? ' and 1 other.' : ' and ' + amount + ' others.'
   }
 }]);
